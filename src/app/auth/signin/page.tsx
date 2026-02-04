9e753bad700f7bbbb84e4dd39ed498904b8ec994
AmaurysDM <amaurysdlsm@gmail.com>
2026-02-04 14:39:15 -0400

    feat: add Discord provider to sign-in page and update image remote patterns

--- src/app/auth/signin/page.tsx
+++ src/app/auth/signin/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { signIn } from "next-auth/react";
-import { FaGithub, FaGoogle } from "react-icons/fa";
+import { FaGithub, FaGoogle, FaDiscord } from "react-icons/fa";
 import VoronoiBackground from "../../components/VoronoiBackground";
 import Image from "next/image";
 
@@ -17,7 +17,13 @@ const providers = [
     id: "google",
     icon: <FaGoogle className="w-5 h-5 mr-2" />,
     color: "btn-error",
-  }
+  },
+  {
+    name: "Discord",
+    id: "discord",
+    icon: <FaDiscord className="w-5 h-5 mr-2" />,
+    color: "btn-primary",
+  },
 ];
 
 export default function SignInPage() {
